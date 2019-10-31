020b156930e124b81772f36fadf4122296ffa932
william-oliveira-da-silva <wyll.hello@gmail.com>
2019-10-31 00:34:44 -0200

    aula 30/10

--- tv-search/src/pages/Pesquisar.js
+++ tv-search/src/pages/Pesquisar.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-
+import logo from '../img/logo.png';
+import Header from '../components/Header';
 
  export default class Pesquisar extends Component {
   render() {
     return (
         <div>
-            Pesquisar
+            <Header
+              enderecoPaginaAnterior = "/"
+              logo={logo}
+              titulo="Tv Search"/>
         </div>
     )
   }
--- tv-search/src/pages/VisualizarSeries.js
+++ tv-search/src/pages/VisualizarSeries.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
-
+import logo from '../img/paginaAnterior.png';
+import Header from '../components/Header';
 
  export default class VisualizarSeries extends Component {
   render() {
     return (
-        <div>
-            VisualizarSeries
-        </div>
+      <div>
+        <Header
+          enderecoPaginaAnterior = "/"
+          logo={logo}
+          titulo="Visualizar Serie"/>
+      </div>
     )
   }
 }
